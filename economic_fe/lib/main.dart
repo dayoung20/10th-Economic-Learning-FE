314dd944253935015fdf39824320dd64ea53790e
dayoung20 <adask13@naver.com>
2024-11-05 20:24:42 +0900

    test

--- economic_fe/lib/main.dart
+++ economic_fe/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Text("getX 적용"),
+      home: const Text("test"),
     );
   }
 }
