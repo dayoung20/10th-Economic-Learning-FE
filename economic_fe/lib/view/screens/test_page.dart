1d1543fc638d851f93bf60facc2c864146f3a1e2
siiion <siwonjeon234@gmail.com>
2025-01-11 12:10:34 +0900

    UI: 레벨테스트 시작하기 화면 디자인 수정사항 반영

--- economic_fe/lib/view/screens/test_page.dart
+++ economic_fe/lib/view/screens/test_page.dart
@@ -27,125 +27,94 @@ class _TestPageState extends State<TestPage> {
       backgroundColor: Palette.background,
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const SizedBox(
-            height: 178,
-          ),
           Padding(
-            padding: const EdgeInsets.only(left: 40),
-            child: SizedBox(
-              height: 78,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '경제학습의 첫걸음,',
-                    style: Palette.cardTitle,
-                  ),
-                  Row(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 178,
+                ),
+                SizedBox(
+                  height: 78,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        '나의 레벨은?',
+                        '경제학습의 첫걸음,',
                         style: Palette.cardTitle,
                       ),
-                      SizedBox(
-                        width: 3,
-                      ),
-                      Image.asset(
-                        "assets/icon.png", width: 35.0, // 원하는 가로 크기
-                        height: 35.0,
+                      Row(
+                        children: [
+                          Text(
+                            '나의 레벨은?',
+                            style: Palette.cardTitle,
+                          ),
+                          const SizedBox(
+                            width: 3,
+                          ),
+                          Image.asset(
+                            "assets/icon.png", width: 35.0, // 원하는 가로 크기
+                            height: 35.0,
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
+                const SizedBox(
+                  height: 44,
+                ),
+                Text(
+                  '학습 수준을 정확히 파악할 수 있도록 \n간단한 테스트를 준비했습니다. \n함께 시작해볼까요?',
+                  style: Palette.subleveltitle,
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: [
+              Center(
+                child: CustomButton(
+                  text: "시작하기",
+                  onPress: () {
+                    controller.clickedTestMultiBtn(context);
+                  },
+                  bgColor: Palette.buttonColorBlue,
+                ),
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 44,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 40),
-            child: Text(
-              '학습 수준을 정확히 파악할 수 있도록 \n간단한 테스트를 준비했습니다. \n함께 시작해볼까요?',
-              style: Palette.subleveltitle,
-            ),
-          ),
-          const SizedBox(
-            height: 175,
-          ),
-          Center(
-            child: CustomButton(
-              text: "레벨테스트 시작하기",
-              onPress: () {
-                controller.clickedTestMultiBtn(context);
-              },
-              bgColor: Palette.buttonColorBlue,
-            ),
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () {
-                controller.clickedAfterBtn(context);
-              },
-              child: SizedBox(
-                child: Text(
-                  '나중에 할래요',
-                  style: TextStyle(
-                    color: Color(0xFF111111),
-                    fontSize: 18,
-                    fontFamily: 'Pretendard Variable',
-                    fontWeight: FontWeight.w600,
-                    height: 1.20,
-                    letterSpacing: -0.45,
+              const SizedBox(
+                height: 12,
+              ),
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    controller.clickedAfterBtn(context);
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      '나중에 시작할래요',
+                      style: TextStyle(
+                        color: Color(0xFF404040),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: -0.35,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          )
-          // CustomButton(
-          //     text: "나중에 할게요",
-          //     onPress: () {
-          //       controller.clickedAfterBtn(context);
-          //     },
-          //     bgColor: Palette.background),
-          // Stack(
-          //   alignment: Alignment.center,
-          //   children: [
-          //     ElevatedButton(
-          //       onPressed: () => {print("onpressed")},
-          //       child: const Text('버튼'),
-          //     ),
-          //     if (true) // 조건부 렌더링은 children 내에서 이루어짐
-          //       AbsorbPointer(
-          //         absorbing: true,
-          //         child: Container(
-          //           color: Colors.transparent, // 버튼이 보이도록 투명 처리
-          //           height: 48,
-          //           width: 100,
-          //         ),
-          //       ),
-          //   ],
-          // ),
+              const SizedBox(
+                height: 69,
+              ),
+            ],
+          ),
         ],
       ),
-      // ElevatedButton(
-      //         onPressed: () => {print("onpressed")},
-      //         child: const Text('버튼'),
-      //       ),
-      //       if (_isDisabled)
-      //         AbsorbPointer(
-      //           absorbing: true,
-      //           child: Container(
-      //             color: Colors.transparent, // 버튼이 보이도록 투명 처리
-      //             height: 48,
-      //             width: 100,
-      //           ),
-      //         ),
     );
   }
 }
