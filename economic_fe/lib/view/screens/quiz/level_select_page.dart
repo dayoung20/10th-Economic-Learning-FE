fc1c8937a8e64ad0e5d75b1558cacea939fd22a7
siiion <siwonjeon234@gmail.com>
2025-01-25 17:57:25 +0900

    MOD: 퀴즈 레벨 선택 UI 수정

--- economic_fe/lib/view/screens/quiz/level_select_page.dart
+++ economic_fe/lib/view/screens/quiz/level_select_page.dart
@@ -34,7 +34,7 @@ class _LevelSelectPageState extends State<LevelSelectPage> {
         icon: Icons.arrow_back_ios_new,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(horizontal: 32),
         child: Center(
           child: Column(
             children: [
@@ -42,7 +42,7 @@ class _LevelSelectPageState extends State<LevelSelectPage> {
                 height: 62,
               ),
               _buildLevelButton(
-                label: 'Beginner(초급)',
+                label: '초급',
                 isSelected: _selectedLevel == 'Beginner',
                 onTap: () {
                   setState(() {
@@ -53,10 +53,11 @@ class _LevelSelectPageState extends State<LevelSelectPage> {
                     // print(controller.selectedLevel);
                   });
                 },
+                isCompleted: true,
               ),
               const SizedBox(height: 16),
               _buildLevelButton(
-                label: 'Intermediate(중급)',
+                label: '중급',
                 isSelected: _selectedLevel == 'Intermediate',
                 onTap: () {
                   setState(() {
@@ -66,10 +67,11 @@ class _LevelSelectPageState extends State<LevelSelectPage> {
                     controller.clickedQuizBtn(context);
                   });
                 },
+                isCompleted: false,
               ),
               const SizedBox(height: 16),
               _buildLevelButton(
-                label: 'Advanced(고급)',
+                label: '고급',
                 isSelected: _selectedLevel == 'Advanced',
                 onTap: () {
                   setState(() {
@@ -79,6 +81,7 @@ class _LevelSelectPageState extends State<LevelSelectPage> {
                     controller.clickedQuizBtn(context);
                   });
                 },
+                isCompleted: false,
               ),
             ],
           ),
@@ -91,27 +94,45 @@ class _LevelSelectPageState extends State<LevelSelectPage> {
     required String label,
     required bool isSelected,
     required VoidCallback onTap,
+    required bool isCompleted,
   }) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: 294,
+        width: MediaQuery.of(context).size.width - 64,
         padding: const EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: isSelected
-              ? const Color(0xFF1EB692)
-              : Colors.white, // 선택 여부에 따라 색상 변경
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(color: Colors.grey),
-        ),
-        child: Text(
-          label,
-          style: TextStyle(
-            fontSize: 18,
-            color:
-                isSelected ? Colors.white : Colors.black, // 선택 여부에 따라 텍스트 색상 변경
-            fontWeight: FontWeight.bold,
+        decoration: ShapeDecoration(
+          color: isSelected ? Palette.buttonColorGreen : Colors.white,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              width: 1,
+              color: isSelected
+                  ? Palette.buttonColorGreen
+                  : const Color(0xFFD9D9D9),
+            ),
+            borderRadius: BorderRadius.circular(12),
           ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? Colors.white : const Color(0xFF111111),
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                height: 1.50,
+                letterSpacing: -0.50,
+              ),
+            ),
+            Icon(
+              isCompleted ? Icons.check_circle : Icons.check_circle_outline,
+              color: isCompleted
+                  ? Palette.buttonColorGreen
+                  : const Color(0xffa2a2a2),
+            )
+          ],
         ),
       ),
     );
