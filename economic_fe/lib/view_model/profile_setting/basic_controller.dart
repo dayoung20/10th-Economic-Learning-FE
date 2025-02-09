832bdb6f32a13d8ee6fac0a988df790d0146cdbc
siiion <siwonjeon234@gmail.com>
2025-02-09 10:40:54 +0900

    FEAT: 프로필 이미지 등록/삭제 api 연동

--- economic_fe/lib/view_model/profile_setting/basic_controller.dart
+++ economic_fe/lib/view_model/profile_setting/basic_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:economic_fe/data/services/date_picker_service.dart';
 import 'package:economic_fe/data/services/image_picker_service.dart';
+import 'package:economic_fe/data/services/remote_data_source.dart';
 import 'package:economic_fe/view_model/profile_setting/profile_setting_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,6 +16,12 @@ class BasicController extends GetxController {
   var nickname = ''.obs;
   var isValid = false.obs;
   var errorMessage = ''.obs;
+
+  // 프로필 이미지
+  var imageId = Rxn<int>();
+
+  // 삭제 모드 활성화 여부
+  var isDeleteMode = false.obs;
 
   // 닉네임 입력 컨트롤러 (키보드 입력 문제 해결)
   final TextEditingController nicknameController = TextEditingController();
@@ -45,6 +54,44 @@ class BasicController extends GetxController {
     final image = await _imagePickerService.showImagePickerDialog(context);
     if (image != null) {
       selectedProfileImage.value = image.path;
+
+      // 이미지 업로드 후 imageId 받아오기
+      int? uploadedImageId =
+          await RemoteDataSource.uploadProfileImage(File(image.path));
+
+      if (uploadedImageId != null) {
+        imageId.value = uploadedImageId; // Rxn<int>로 변경
+        imageId.refresh(); // UI에 반영
+
+        // ProfileSettingController에 imageId 업데이트
+        Get.find<ProfileSettingController>()
+            .updateProfileField('imageId', imageId.value);
+        Get.snackbar('성공', '프로필 이미지가 업데이트되었습니다.');
+      } else {
+        Get.snackbar('실패', '프로필 이미지 등록에 실패하였습니다.');
+      }
+    }
+  }
+
+  /// 프로필 이미지 삭제 메서드
+  Future<void> deleteProfileImage() async {
+    final ProfileSettingController profileController =
+        Get.find<ProfileSettingController>();
+
+    if (imageId.value != null) {
+      bool isDeleted = await RemoteDataSource.deleteImage(imageId.value!);
+
+      if (isDeleted) {
+        // 프로필 이미지 초기화
+        imageId.value = null; // imageId를 null로 설정
+        selectedProfileImage.value = null;
+        imageId.refresh(); // UI 반영
+        profileController.updateProfileField('imageId', null);
+
+        Get.snackbar('성공', '프로필 이미지가 삭제되었습니다.');
+      } else {
+        Get.snackbar('오류', '이미지 삭제에 실패했습니다.');
+      }
     }
   }
 
@@ -65,7 +112,7 @@ class BasicController extends GetxController {
 
     _updateProfileField('nickname', value);
 
-    // ✅ 저장 버튼 상태 업데이트
+    // 저장 버튼 상태 업데이트
     _updateSaveButtonState();
   }
 
@@ -80,7 +127,7 @@ class BasicController extends GetxController {
     // UI 강제 업데이트
     selectedGender.refresh();
 
-    // ✅ 저장 버튼 상태 업데이트
+    // 저장 버튼 상태 업데이트
     _updateSaveButtonState();
   }
 
@@ -92,7 +139,7 @@ class BasicController extends GetxController {
           '${pickedDate.year}-${pickedDate.month.toString().padLeft(2, '0')}-${pickedDate.day.toString().padLeft(2, '0')}';
       _updateProfileField('birthDate', selectedBirthday.value);
 
-      // ✅ 저장 버튼 상태 업데이트
+      // 저장 버튼 상태 업데이트
       _updateSaveButtonState();
     }
   }
