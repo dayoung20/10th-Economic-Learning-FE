1f2bd6f103cd874d8f887dc39c539115ac8cb75a
dayoung20 <adask13@naver.com>
2025-02-20 10:20:50 +0900

    feat: OX 퀴즈 완료

--- economic_fe/lib/view/widgets/quiz_card.dart
+++ economic_fe/lib/view/widgets/quiz_card.dart
@@ -232,20 +232,30 @@ class _QuizCardState extends State<QuizCard> {
                   onPress: controller.isNextButtonEnabled
                       ? widget.isQuiz
                           ? () {
-                              if (widget.isLast) {
-                                widget.onFinishTest!();
-                              } else {
-                                // 퀴즈에서 다음 문제 버튼 클릭 시 함수
-                                controller.clickCheckBtn.value = true;
-                                controller.isCorrectAnswer.value =
-                                    // controller.selectedOption.value == 0 ? 1 : 2;
-                                    controller.isCorrect.value ? 1 : 2;
+                              // if (widget.isLast) {
+                              //   widget.onFinishTest!();
+                              // } else {
+                              //   // 퀴즈에서 확인 버튼 클릭 시 함수
+                              //   controller.clickCheckBtn.value = true;
+                              //   controller.isCorrectAnswer.value =
+                              //       // controller.selectedOption.value == 0 ? 1 : 2;
+                              //       controller.isCorrect.value ? 1 : 2;
 
-                                // 답안 제출
-                                controller.postSubmitQuiz(widget.quizId!,
-                                    controller.selectedOption.value);
-                                // print("se : ${controller.selectedOption.value}");
-                              }
+                              //   // 답안 제출
+                              //   controller.postSubmitQuiz(widget.quizId!,
+                              //       controller.selectedOption.value);
+                              //   // print("se : ${controller.selectedOption.value}");
+                              // }
+                              // 퀴즈에서 다음 문제 버튼 클릭 시 함수
+                              controller.clickCheckBtn.value = true;
+                              controller.isCorrectAnswer.value =
+                                  // controller.selectedOption.value == 0 ? 1 : 2;
+                                  controller.isCorrect.value ? 1 : 2;
+
+                              // 답안 제출
+                              controller.postSubmitQuiz(widget.quizId!,
+                                  controller.selectedOption.value);
+                              // print("se : ${controller.selectedOption.value}");
                             }
                           : widget.isLast
                               ? () {
@@ -260,6 +270,8 @@ class _QuizCardState extends State<QuizCard> {
                                       ? widget.onOptionSelected!(
                                           controller.selectedOption.value)
                                       : null;
+                                  //새로 추가됨
+                                  controller.selectedOption.value = -1;
                                 }
                       : null,
                   bgColor: controller.isNextButtonEnabled
@@ -393,15 +405,17 @@ class _QuizCardState extends State<QuizCard> {
                           text: widget.isLast ? '종료하기' : '다음 문제',
                           onPress: () {
                             if (widget.isLast) {
-                              //마지막 문제가 아닐때
+                              //마지막 문제dlf
                               // widget.onOptionSelected != null
                               //     ? widget.onOptionSelected!(
                               //         controller.selectedOption.value)
                               //     : null;
+                              widget.onFinishTest!();
                             } else {
                               print("next ::");
                               widget.onNextQuizBtn!();
                               controller.clickCheckBtn.value = false;
+                              controller.selectedOption.value = -1;
                             }
                           },
                           bgColor: controller.isCorrect.value
